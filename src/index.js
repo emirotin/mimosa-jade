cc6e6b7afcf9882ad1e7f0e1c5e58cf6810d0a02
Eugene Mirotin <emirotin@gmail.com>
2014-03-29 21:28:48 +0300

    added static option

--- src/index.js
+++ src/index.js
@@ -4,34 +4,61 @@ var path = require( "path" )
   , config = require( "./config" )
   , getExtensions = function ( mimosaConfig ) {
     return mimosaConfig.jade.extensions;
+  }
+  , getIsStatic = function ( mimosaConfig ) {
+    return !!mimosaConfig.jade.static;
+  }
+  , getStaticContext = function ( mimosaConfig ) {
+    if ( !getIsStatic( mimosaConfig ) ) {
+      return null;
+    }
+    var context = mimosaConfig.jade.staticContext;
+    if (typeof context === 'function') {
+      context = context();
+    }
+    return context || null;
   };
 
 var prefix = function ( mimosaConfig, libraryPath ) {
+  var isStatic = getIsStatic( mimosaConfig ),
+    res;
   if ( mimosaConfig.template.wrapType === "amd" ) {
-    return "define(['" + libraryPath + "'], function (jade){ var templates = {};\n";
-  } else {
-    if ( mimosaConfig.template.wrapType === "common" ) {
-      return "var jade = require('" + mimosaConfig.template.commonLibPath + "');\nvar templates = {};\n";
+    res = "define([";
+    if ( !isStatic ) {
+      res += "'" + libraryPath + "'";
     }
+    res += "], function (";
+    if ( !isStatic ) {
+      res += "jade";
+    }
+    res += "){ var templates = {};\n";
+  } else if ( mimosaConfig.template.wrapType === "common" ) {
+    res = "";
+    if ( !isStatic ) {
+      res += "var jade = require('" + mimosaConfig.template.commonLibPath + "');\n";
+    }
+    res += "var templates = {};\n";
+  } else {
+    res = "var templates = {};\n";
   }
-
-  return "var templates = {};\n";
+  return res;
 };
 
 var suffix = function ( mimosaConfig ) {
   if ( mimosaConfig.template.wrapType === "amd" ) {
     return "return templates; });";
+  } else if ( mimosaConfig.template.wrapType === "common" ) {
+    return "\nmodule.exports = templates;";
   } else {
-    if ( mimosaConfig.template.wrapType === "common" ) {
-      return "\nmodule.exports = templates;";
-    }
+    return "";
   }
-
-  return "";
 };
 
 var compile = function ( mimosaConfig, file, cb ) {
-  var error, output;
+  var error
+    , output
+    , isStatic = getIsStatic( mimosaConfig )
+    , staticContext;
 
   try {
     var opts = {
@@ -39,7 +66,14 @@ var compile = function ( mimosaConfig, file, cb ) {
       filename: file.inputFileName
     };
 
-    output = mimosaConfig.jade.lib.compileClient( file.inputFileText, opts);
+    if ( !isStatic ) {
+      output = mimosaConfig.jade.lib.compileClient( file.inputFileText, opts);
+    } else {
+      output = mimosaConfig.jade.lib.compile( file.inputFileText, opts);
+      staticContext = getStaticContext( mimosaConfig );
+      output = output( staticContext );
+      output = JSON.stringify(output);
+    }
   } catch ( err ) {
     error = err;
   }
